56c4dd00dae7a404dc0f44ab03790b3ae42ad54b
kunal kumar <kunalkr42011@gmail.com>
2026-04-17 23:26:32 +0530

    update

--- api/index.py
+++ api/index.py
@@ -1,3 +1,10 @@
+import os
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(ROOT))
+
 from app import app
 
 # Vercel Python runtime will use the Flask app object from this module.
